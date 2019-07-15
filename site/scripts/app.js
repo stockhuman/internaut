d6613bcbad0c50c4370fc20a0aa95fbc53a66c28
Michael Hemingway <mpghemingway@gmail.com>
2019-07-14 21:01:20 -0400

    New: Jars productivity visualiser!

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -4,73 +4,17 @@
  */
 
 import Navigation from './components/nav'
+import Logger from './components/logger'
 
 const nav = new Navigation()
 
 nav.mount(() => {
+	nav.icon()
 	nav.colors()
 })
 
+const logroot = document.querySelector('.stats')
 
-// // Smoothstate JQ plugin
-// $('#container').smoothState({
-// 	blacklist: '.no-smoothState',
-// 	onStart : {
-// 		duration: 450,
-// 		// Alterations to the page
-// 		render: function () {
-// 			// Quickly toggles a class and restarts css animations
-// 			body.toggleClass('is-exiting');
-// 		}
-// 	},
-// 	onAfter: function () {
-// 		$.readyFn.execute();
-// 		navColors(); // color the nav
-// 		resetNav(); // hide the transitioner / close the nav
-
-// 		if (window.location.origin == window.location.href) {
-// 			document.getElementById('home-canvas').style.opacity = 1;
-// 		} else {
-// 			document.getElementById('home-canvas').style.opacity = 0.2;
-// 		}
-
-// 		// fix collection styling
-// 		if ((window.location.href.indexOf("collection") > -1)) {
-// 			body.addClass('collection');
-// 		} else { body.removeClass('collection'); }
-
-// 		body.toggleClass('is-exiting');
-// 	}
-// })
-
-// // add nav links to smoothState
-// $(".nav-list-item a, #nav-home-icon a").click(function(e) {
-// 	e.preventDefault();
-// 	var c = $("#container").smoothState().data("smoothState"),
-// 			l = $(this).attr("href");
-// 	c.load(l);
-// });
-
-// // MISC_____________________________________
-
-// // the projects page
-// $(document).ready(function () {
-// 	var filterBtnGroup = $('#project-filters a');
-
-// 	var projects = $('.isotope').isotope({
-// 		itemSelector: '.item',
-// 		columnWidth: '.project',
-// 		percentPosition: true
-// 	});
-
-// 	// filter the isotope layout
-// 	filterBtnGroup.on('click', function (e) {
-// 		e.preventDefault();
-// 		var filterVal = $(this).attr('data-filter');
-// 		projects.isotope({filter: filterVal});
-// 	});
-
-// 	projects.imagesLoaded().progress( function() {
-// 	  projects.isotope('layout');
-// 	});
-// })
+if (logroot) {
+	new Logger(logroot)
+}
---
+++ site/scripts/components/logger.js
@@ -0,0 +1,113 @@
+
+export default class Logger {
+	constructor (root) {
+		this.root = root
+		this.project = root.attributes['data-project'].nodeValue
+		this.query = this.makeQuery()
+
+
+		this.request()
+	}
+
+	_date (date) {
+		date = date ? date : new Date()
+
+		let str = ''
+		str += date.getFullYear()
+		str += ('' + date.getMonth()).padStart(2, '0')
+		str += '00'
+
+		return str
+	}
+
+	_fromSQL(date) {
+		if (!date) return new Date()
+
+		date = String(date)
+
+		return new Date(
+			date.substring(0, 4), // year
+			Number(date.substring(5, 6)) - 1, // month, zero indexed (???)
+			date.substring(7)
+		)
+	}
+
+	makeQuery() {
+		// get most recent records of all activities
+		if (this.project === '*') {
+			return `date,gt,${this._date()}`
+		} else {
+			return `project,eq,${this.project}`
+		}
+	}
+
+	request () {
+		const api = 'https://api.arthem.co/jars/v1/records/beans/'
+		fetch(`${api}?filter=${this.query}&exclude=ID,task,comment`)
+			.then(response => response.json())
+			.then(json => this.render(json))
+	}
+
+
+	render (data) {
+		// returns distance in days
+		const dist = (a, b) => Math.abs(Math.round(((a - b) / (1000 * 60 * 60 * 24))))
+
+		// construct a single log <rect />
+		const day = day => {
+			let y = 0
+			let x = ratio * dist(furthest, this._fromSQL(day.date))
+			let w = ratio * day.hours
+
+			// determine y given svg viewBox height of 105
+			switch (day.tod) {
+				case 'em': y = 10; break // in the early morning
+				case 'm': y = 20; break // in the morning
+				case 'md': y = 30; break // around midday
+				case 'an': y = 40; break // in the afternoon
+				case 'ev': y = 50; break // in the evening
+				case 'n': y = 60; break // around nighttime
+				case 'ln': y = 70; break // well past sundown
+			}
+
+			return `<rect x="${x}" y="${y}" width="${w}" height="3" rx="1.5" class="${day.category}"/>`
+		}
+
+		const todstrs = ['em','m','md','an','ev','n','ln']
+
+		let svg = ''
+		let fmonth = new Date().getMonth()
+		let furthest = new Date(new Date().getFullYear(), fmonth - 2)
+
+
+		let ratio = 600 / dist(furthest, new Date())
+
+		// create 'time of day' labels
+		for (let i = 0; i < 7; i++) {
+			svg += `<text x="0" y="${i * 10 + 15}">${todstrs[i]}</text>`
+		}
+
+		// append logs to svg
+		data.records.forEach(el => svg += day(el))
+
+		// discover modal time of day
+		// via https://stackoverflow.com/questions/52898456/
+		const mode = a => {
+			return Object.values(
+				a.reduce((count, e) => {
+					if (!(e in count)) count[e] = [0, e]
+					count[e][0]++
+					return count
+				}, {})
+			).reduce((a, v) => v[0] < a[0] ? a : v, [0, null])[1]
+		}
+
+		// most commonly entered time of day for work
+		const modalTod = document.createElement('p')
+		modalTod.innerHTML =
+			`Scale: 2 Months. Mode: ${mode(data.records.map(item => item.tod))}.`
+
+		this.root.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 105">${svg}</svg>`
+		this.root.appendChild(modalTod)
+	}
+}
