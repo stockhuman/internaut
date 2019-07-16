edcbc1f009e5f99cc80ecd6f9b5a71b7f8293972
Michael Hemingway <mpghemingway@gmail.com>
2019-07-15 22:56:37 -0400

    New: Load the full-res / color image on demand

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -6,6 +6,8 @@
 import Navigation from './components/nav'
 import Logger from './components/logger'
 
+import { allImages } from './components/upres'
+
 const nav = new Navigation()
 
 nav.mount(() => {
@@ -18,3 +20,20 @@ const logroot = document.querySelector('.stats')
 if (logroot) {
 	new Logger(logroot)
 }
+
+const pictures = allImages(document)
+
+// onclick, change out low-res image for hi-res
+pictures.forEach(image => {
+	image.node.addEventListener('click', () => {
+		if (image.src.includes('dither-')) {
+			let undithered = image.src.replace('dither-', '').replace('.png', '')
+			if (image.type === 'bg') {
+				image.node.style.backgroundImage = `url(${undithered})`
+				image.node.style.mixBlendMode = 'initial'
+			} else {
+				image.node.src = undithered
+			}
+		}
+	})
+})
