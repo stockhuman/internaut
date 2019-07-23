a742dcb67dadcf3b9f6342b998bbeb5780d164f8
Michael Hemingway <mpghemingway@gmail.com>
2019-07-23 16:42:52 -0400

    Misc styling

--- site/scripts/home.js
+++ site/scripts/home.js
@@ -24,41 +24,10 @@
 // window.addEventListener('resize', resize, false)
 // resize()
 
-// const num = 100
-// const position = new Float32Array(num * 3)
-// const random = new Float32Array(num * 4)
-// for (let i = 0; i < num; i++) {
-// 	position.set([Math.random(), Math.random(), Math.random()], i * 3)
-// 	random.set([Math.random(), Math.random(), Math.random(), Math.random()], i * 4)
-// }
-
-// const geometry = new Geometry(gl, {
-// 	position: {size: 3, data: position},
-// 	random: {size: 4, data: random},
-// })
-
-// const program = new Program(gl, {
-// 	vertex: ParticlesShader.vertex,
-// 	fragment: ParticlesShader.fragment,
-// 	uniforms: {
-// 		uTime: {value: 0},
-// 	},
-// 	transparent: true,
-// 	depthTest: false,
-// })
-
-// // Make sure mode is gl.POINTS
-// const particles = new Mesh(gl, {mode: gl.POINTS, geometry, program})
-
 // requestAnimationFrame(update)
 
 // function update(t) {
 // 	requestAnimationFrame(update)
-// 	// add some slight overall movement to be more interesting
-// 	particles.rotation.x = Math.sin(t * 0.0002) * 0.1
-// 	particles.rotation.y = Math.cos(t * 0.0005) * 0.15
-// 	particles.rotation.z += 0.01
-// 	program.uniforms.uTime.value = t * 0.001
-// 	renderer.render({scene: particles, camera})
+// 	renderer.render({camera})
 // 	controls.update()
 // }
