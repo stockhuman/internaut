6c9d97420636f28988be3b6e039332353733e1ef
Michael Hemingway <mpghemingway@gmail.com>
2019-09-04 14:32:30 -0400

    Refactored out settings from app.js

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -5,87 +5,23 @@
 
 import Navigation from './components/nav'
 import Logger from './components/logger'
-
-import { allImages } from './components/upres'
+import Settings from './components/settings'
 
 const nav = new Navigation()
-const pictures = allImages(document)
+const settings = new Settings()
+
 const logroot = document.querySelector('.stats')
-const tgl_darkmode = document.getElementById('tgl-dark-mode')
-const tgl_fullcolor = document.getElementById('tgl-full-color')
-
-let theme = localStorage.getItem('theme')
-let fullcolor = localStorage.getItem('fullcolor')
-
-nav.mount(() => nav.colors())
 if (logroot) new Logger(logroot) // initialises logger
 
-// via https://web.dev/prefers-color-scheme/
-if (!theme) {
-	if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-		localStorage.setItem('theme', 'dark')
-	} else {
-		localStorage.setItem('theme', 'light')
-	}
-}
-if (!fullcolor) localStorage.setItem('fullcolor', false)
-tgl_darkmode.checked = theme === 'light' ? false : true
-tgl_fullcolor.checked = fullcolor === 'true' ? true : false
-
-const toggleColor = () => {
-	if (tgl_fullcolor.checked) {
-		localStorage.setItem('fullcolor', true)
-		pictures.forEach(image => undither(image))
-	} else {
-		localStorage.setItem('fullcolor', false)
-	}
-}
-
-const toggleDarkTheme = () => {
-	if (tgl_darkmode.checked) {
-		localStorage.setItem('theme', 'dark')
-		document.body.classList.add('dark-theme')
-	} else {
-		localStorage.setItem('theme', 'light')
-		document.body.classList.remove('dark-theme')
-	}
-}
-
-const undither = image => {
-	if (image.src.includes('dither-')) {
-		let undithered = image.src.replace('dither-', '').replace('.png', '')
-		if (image.type === 'bg') {
-			image.node.style.backgroundImage = `url(${undithered})`
-			image.node.style.mixBlendMode = 'initial'
-		} else {
-			image.node.src = undithered
-		}
-	}
-}
-
-if (fullcolor == 'true') {
-	pictures.forEach(image => undither(image))
-} else {
-	// onclick, change out low-res image for hi-res
-	pictures.forEach(image => {
-		image.node.addEventListener('click', () => {
-			undither(image)
-		})
-	})
-}
-
-if (theme == 'dark') {
-	document.body.classList.add('dark-theme')
-} else {
-	document.body.classList.remove('dark-theme')
-}
+document.addEventListener('DOMContentLoaded', () => {
+	settings.mount()
+	nav.mount()
+	nav.colors()
+})
 
 // Class for mouse movement, hides thick border used for tabbing
 let mm = () => {
 	document.body.classList.add('mn')
 	document.removeEventListener('mousemove', mm)
 }
-
-tgl_darkmode.addEventListener('change', toggleDarkTheme)
-tgl_fullcolor.addEventListener('change', toggleColor)
 document.addEventListener('mousemove', mm)
---
+++ site/scripts/components/settings.js
@@ -0,0 +1,89 @@
+import { allImages } from './upres'
+
+export default class Settings {
+	constructor () {
+		this.theme = localStorage.getItem('theme')
+		this.dithered = localStorage.getItem('dithered')
+		this.pictures = allImages(document)
+
+		// via https://web.dev/prefers-color-scheme/
+		if (!this.theme) {
+			if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+				localStorage.setItem('theme', 'dark')
+				this.theme = 'dark'
+			} else {
+				localStorage.setItem('theme', 'light')
+				this.theme = 'light'
+			}
+		}
+
+		if (!this.dithered) localStorage.setItem('dithered', false)
+
+		this.mount = this.mount.bind(this)
+		this.test = this.test.bind(this)
+		this.toggleDarkTheme = this.toggleDarkTheme.bind(this)
+		this.toggleColor = this.toggleColor.bind(this)
+	}
+
+	mount () {
+		this.tglTheme = document.getElementById('tgl-dark-mode')
+		this.tglDither = document.getElementById('tgl-full-color')
+		this.tglTheme.addEventListener('change', this.toggleDarkTheme)
+		this.tglDither.addEventListener('change', this.toggleColor)
+
+		this.tglTheme.checked = this.theme === 'light' ? false : true
+		this.tglDither.checked = this.dithered === 'true' ? true : false
+
+		this.test()
+	}
+
+	test () {
+		if (this.dithered == 'true') {
+			this.pictures.forEach(image => undither(image))
+		} else {
+			// onclick, change out low-res image for hi-res
+			this.pictures.forEach(image => {
+				image.node.addEventListener('click', () => {
+					this.undither(image)
+				})
+			})
+		}
+
+		if (this.theme == 'dark') {
+			document.body.classList.add('dark-theme')
+		} else {
+			document.body.classList.remove('dark-theme')
+		}
+	}
+
+	toggleColor () {
+		if (this.tglDither.checked) {
+			localStorage.setItem('dithered', true)
+			this.pictures.forEach(image => this.undither(image))
+		} else {
+			localStorage.setItem('dithered', false)
+		}
+	}
+
+	toggleDarkTheme () {
+		if (this.tglTheme.checked) {
+			localStorage.setItem('theme', 'dark')
+			document.body.classList.add('dark-theme')
+		} else {
+			localStorage.setItem('theme', 'light')
+			document.body.classList.remove('dark-theme')
+		}
+	}
+
+	undither (image) {
+		if (image.src.includes('dither-')) {
+			let undithered = image.src.replace('dither-', '').replace('.png', '')
+			if (image.type === 'bg') {
+				image.node.style.backgroundImage = `url(${undithered})`
+				image.node.style.mixBlendMode = 'initial'
+			} else {
+				image.node.src = undithered
+			}
+		}
+	}
+}
