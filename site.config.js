dede34f894037dc2fab3069071a759a977e06fb4
Michael Hemingway <mpghemingway@gmail.com>
2021-04-22 21:07:11 -0400

    Discontinue _data.json use

--- site.config.js
+++ site.config.js
@@ -3,6 +3,7 @@ const fse = require('fs-extra')
 const frontMatter = require('front-matter')
 
 const collection = []
+const projects = []
 
 // via this excellent gist: https://gist.github.com/kethinov/6658166
 const walk = (dir, fileList = []) => {
@@ -24,8 +25,23 @@ collectionData.forEach(page => {
   }
 })
 
-// allows for custom ordering of projects, and omission from project page
-const projects = require('./site/pages/projects/_data.json')
+const projectData = walk(`./site/pages/projects/`)
+projectData.forEach((page) => {
+	if (page.endsWith('.md')) { // skip index
+		const data = fse.readFileSync(page, 'utf-8')
+		const pageData = frontMatter(data)
+    const slug = path.basename(page, '.md')
+
+		projects.push({
+			path: slug,
+			page: { slug, ...pageData.attributes }
+		})
+	}
+})
+projects.sort((a, b) => b.page.year - a.page.year)
+
+// // allows for custom ordering of projects, and omission from project page
+// const projects = require('./site/pages/projects/_data.json')
 
 module.exports = {
   build: {
