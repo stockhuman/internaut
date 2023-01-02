77b1a3b3a0801d1e177c240f1783edbc81a3f7c6
Michael Hemingway <mikemingway@gmail.com>
2023-01-02 13:47:23 -0500

    Run prettier

--- site/scripts/home.js
+++ site/scripts/home.js
@@ -1,52 +1,50 @@
 // setup
-const canvas = document.body.appendChild(
-	document.createElement("canvas")
-)
-const context = canvas.getContext("2d")
+const canvas = document.body.appendChild(document.createElement('canvas'))
+const context = canvas.getContext('2d')
 
 window.addEventListener('resize', size)
 document.addEventListener('click', () => {
-	canvas.classList = 'fade'
-	setTimeout(() => {
-		size()
-		canvas.classList = ''
-	}, 1500);
+  canvas.classList = 'fade'
+  setTimeout(() => {
+    size()
+    canvas.classList = ''
+  }, 1500)
 })
 size()
 
 // use these alignment properties for "better" positioning
-context.textAlign = "center";
-context.textBaseline = "middle";
+context.textAlign = 'center'
+context.textBaseline = 'middle'
 context.font = 20 + Math.random() * 100 + 'px serif'
-context.fillText('😍', window.innerWidth/2, 200)
+context.fillText('😍', window.innerWidth / 2, 200)
 
 // give some time to read intro copy
 setTimeout(() => {
-	setInterval(() => {
-		draw({
-			pageX: Math.random() * window.innerWidth,
-			pageY: Math.random() * document.documentElement.scrollHeight
-		})
-	}, 1000 * Math.random() + 100);
-}, 1000);
+  setInterval(() => {
+    draw({
+      pageX: Math.random() * window.innerWidth,
+      pageY: Math.random() * document.documentElement.scrollHeight,
+    })
+  }, 1000 * Math.random() + 100)
+}, 1000)
 
 // draw the emoji
-function draw (e) {
-	let emoji = ['😜','😂','😍']
-	let d = emoji[Math.floor(Math.random() * emoji.length)]
-	context.rotate(Math.PI / Math.random())
-	context.font = 6 + Math.random() * 100 + 'px serif'
-	context.fillText(d, e.pageX, e.pageY)
+function draw(e) {
+  let emoji = ['😜', '😂', '😍']
+  let d = emoji[Math.floor(Math.random() * emoji.length)]
+  context.rotate(Math.PI / Math.random())
+  context.font = 6 + Math.random() * 100 + 'px serif'
+  context.fillText(d, e.pageX, e.pageY)
 }
 
 // resize canvas
-function size () {
-	let dpr = window.devicePixelRatio || 1
-	canvas.width = window.innerWidth * dpr
-	canvas.height = document.documentElement.scrollHeight * dpr
+function size() {
+  let dpr = window.devicePixelRatio || 1
+  canvas.width = window.innerWidth * dpr
+  canvas.height = document.documentElement.scrollHeight * dpr
 
-	canvas.style.height = '100%'
-	canvas.style.width = '100%'
+  canvas.style.height = '100%'
+  canvas.style.width = '100%'
 
-	context.scale(dpr, dpr)
+  context.scale(dpr, dpr)
 }
